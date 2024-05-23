c5b3c9a55a7ef178d03f22a29388608630f68a15
Dev <100162367+pawyercito@users.noreply.github.com>
2024-05-23 00:36:50 -0400

    Update contactCreateController.js

--- controllers/contacts/contactCreateController.js
+++ controllers/contacts/contactCreateController.js
@@ -3,23 +3,28 @@ const Contact = require('../../models/Contact');
 
 exports.createContact = async (req, res) => {
   try {
-    // Verificar si ya existe un contacto con el mismo nombre y número de teléfono para el mismo usuario
-    const existingContact = await Contact.findOne({
-      name: req.body.name,
-      phoneNumber: req.body.phoneNumber,
+    // Verificar si ya existe un contacto con algún número de teléfono duplicado para el mismo usuario
+    const existingContacts = await Contact.find({
       userId: req.user._id
-    });
+    }).lean(); // Usamos lean() para evitar la serialización completa de Mongoose
 
-    if (existingContact) {
-      return res.status(400).json({ msg: 'Este contacto ya existe.' });
+    const duplicatePhoneNumbers = existingContacts.flatMap(contact =>
+      contact.phoneNumbers.map(phoneNumber => ({
+        type: phoneNumber.type,
+        number: phoneNumber.number
+      }))
+    ).filter((phoneNumber, index, self) =>
+      index!== self.findIndex(t => (
+        t.type === phoneNumber.type && t.number === phoneNumber.number
+      ))
+    );
+
+    if (duplicatePhoneNumbers.length > 0) {
+      return res.status(400).json({ msg: 'Existen números de teléfono duplicados.' });
     }
 
-    // Si no existe, crear un nuevo contacto con los datos del cuerpo de la solicitud y el userId del usuario autenticado
-    const newContact = new Contact({...req.body, userId: req.user._id });
+    // Si no existen números de teléfono duplicados, crear un nuevo contacto con los datos del cuerpo de la solicitud y el userId del usuario autenticado
+    const newContact = new Contact(req.body);
     await newContact.save();
     res.status(201).json({ msg: 'Contacto creado exitosamente', contact: newContact });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ msg: 'Error al crear el contacto' });
-  }
-};
