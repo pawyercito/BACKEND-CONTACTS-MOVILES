bd82f6382e9fc21727f4e2626342853eec742ef0
Marcial <eliohernanco@gmail.com>
2024-06-05 21:22:53 -0400

    fix

--- routes/favorite.js
+++ routes/favorite.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { addFavorite } = require('../controllers/favorites/favoriteAddController');
+const { addFavorites } = require('../controllers/favorites/favoriteAddController');
 const { listFavorites } = require('../controllers/favorites/favoriteListController');
 const { removeFavorite } = require('../controllers/favorites/favoriteDeleteController');
 const authenticateUser = require('../middleware_auth');
 
 router.use(authenticateUser);
 
-router.post('/add-favorite', authenticateUser, addFavorite);
+router.post('/add-favorite', authenticateUser, addFavorites);
 router.post('/remove-favorite', authenticateUser, removeFavorite);
 router.get('/list-favorites', authenticateUser, listFavorites);
 
