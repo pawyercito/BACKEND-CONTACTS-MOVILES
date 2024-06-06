32be496ca2260dac7429192265d9161ffafcfb94
Marcial <eliohernanco@gmail.com>
2024-06-05 21:17:57 -0400

    fix

--- controllers/favorites/favoriteAddController.js
+++ controllers/favorites/favoriteAddController.js
@@ -1,27 +1,30 @@
 const Favorite = require('../../models/Favorite');
 const { findContactById } = require('../../controllers/contacts/contactUtils');
 
-exports.addFavorite = async (req, res) => {
+exports.addFavorites = async (req, res) => {
   try {
-    const { userId, contactId } = req.body;
-    
-    // Verificar si el contacto ya está en la lista de favoritos
-    const existingFavorite = await Favorite.findOne({ userId, contactId });
-    if (existingFavorite) {
-      return res.status(400).json({ msg: 'Este contacto ya está en la lista de favoritos' });
+    const { userId, contactIds } = req.body; // Asumiendo que ahora contactIds es un array
+
+    // Iterar sobre cada ID de contacto
+    for (const contactId of contactIds) {
+      // Verificar si el contacto ya está en la lista de favoritos
+      const existingFavorite = await Favorite.findOne({ userId, contactId });
+      if (existingFavorite) {
+        continue; // Si ya existe, continuar con el siguiente contacto
+      }
+
+      const contact = await findContactById(contactId); // Asegúrate de que esta función maneje correctamente los casos donde el contacto no se encuentra
+      const favorite = new Favorite({ userId, contactId });
+
+      await favorite.save();
     }
 
-    const contact = await findContactById(contactId);
-    const favorite = new Favorite({ userId, contactId });
-
-    await favorite.save();
-
     res.status(201).json({
-      msg: `${contact.name} ${contact.lastName} ha sido agregado a favoritos exitosamente`,
-      data: favorite
+      msg: `Los contactos han sido agregados a favoritos exitosamente`,
+      data: contactIds.map(id => ({ id })) // Devolver una respuesta simplificada o más detallada según tus necesidades
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ msg: 'Error al agregar el contacto a favoritos', error: error.message });
+    res.status(500).json({ msg: 'Error al agregar los contactos a favoritos', error: error.message });
   }
 };
